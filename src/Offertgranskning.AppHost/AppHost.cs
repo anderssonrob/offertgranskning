8b0dc5a738dfff7f30a153298ae9a6a13ff14a65
anderssonrob <robertjackie.andersson@gmail.com>
2025-08-16 23:06:51 +0700

    add service-bus emulator

--- src/Offertgranskning.AppHost/AppHost.cs
+++ src/Offertgranskning.AppHost/AppHost.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Offertgranskning_API>("api");
+var sb = builder.AddAzureServiceBus("servicebus")
+    .RunAsEmulator(em => em.WithLifetime(ContainerLifetime.Persistent))
+    .AddServiceBusQueue("offers-metadata", "offers-metadata");
+
+builder.AddProject<Projects.Offertgranskning_API>("api")
+    .WithReference(sb)
+    .WaitFor(sb);
 
 builder.Build().Run();
